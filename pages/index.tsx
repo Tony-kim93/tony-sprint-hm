3373af67dd938f30ad7b84655492bd9f63c42c0e
Tony <Tony@tucks-MacBook-Pro.local>
2021-06-01 16:10:10 +0900

    start project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import ForTest from '../src/component/ForTest';
 
 export default function Home() {
-  return (
-    <div>
-      <div>we can do it</div>
-      <ForTest />
-    </div>
-  );
+  return <title>Tony Sprint-hm</title>;
 }
